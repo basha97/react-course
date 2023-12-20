3e6c1aa40fff04e660d3577e5caef362b9b32fb0
basha97 <bashaadil2727@gmail.com>
2023-12-20 07:52:47 +0530

    creating more component

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -49,12 +49,30 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
     </div>
   );
+}
+
+function Footer() {
+  return <footer>{new Date().toLocaleTimeString} We're currently open</footer>;
 }
 
 function Pizza() {
