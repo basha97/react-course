2b6ed12dd05fd93692707b0bbfd1c4b5637ea4fb
basha97 <bashaadil2727@gmail.com>
2023-12-21 07:49:42 +0530

    conditional rendering with &&

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -96,10 +96,17 @@ function Footer() {
   const openHour = 6;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
-  console.log(isOpen);
-  // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
-  // else alert("Sorry we're closed");
-  return <footer className="footer">{hour} We're currently open</footer>;
+
+  return (
+    <footer className="footer">
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
+    </footer>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
