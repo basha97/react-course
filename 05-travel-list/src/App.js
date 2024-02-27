4f81dd2fe82bd6cdc42828f2dc864b12b5fa411a
basha97 <bashaadil2727@gmail.com>
2024-02-27 18:04:06 +0530

    Buiilding a form and handling submission

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -18,10 +18,23 @@ function Logo() {
   return <h1>🏝 Far Away 🧳</h1>;
 }
 function Form() {
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
@@ -30,7 +43,7 @@ function PackingList() {
     <div className="list">
       <ul>
         {initialItems.map((item) => (
-          <Item item={item} />
+          <Item item={item} key={item.id} />
         ))}
       </ul>
     </div>
